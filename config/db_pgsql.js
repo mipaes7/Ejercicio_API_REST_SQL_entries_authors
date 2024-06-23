a015944d135f5f9afe3fa131c4a2d8656da28988
Miguel Pardal <miguelpardalesparis@gmail.com>
2024-06-23 19:45:42 +0200

    validatorMD added

--- config/db_pgsql.js
+++ config/db_pgsql.js
@@ -5,26 +5,26 @@ const { Pool } = require('pg');
 // Datos de conexión
 
 // PARA USAR CON RENDER
-// const pool = new Pool({
-//   host: 'dpg-cpop272j1k6c73ac5aog-a.frankfurt-postgres.render.com',
-//   user: 'demonodesql_user',
-//   port: '5432',
-//   database: 'demonodesql',
-//   password: 'F9Yd72thuj4tr6wojw6YVDAUVLqd6Fx0',
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+const pool = new Pool({
+  host: 'dpg-cpop272j1k6c73ac5aog-a.frankfurt-postgres.render.com',
+  user: 'demonodesql_user',
+  port: '5432',
+  database: 'demonodesql',
+  password: 'F9Yd72thuj4tr6wojw6YVDAUVLqd6Fx0',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 
 // PARA USAR EN LOCAL CON DOCKER
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    port: '5432',
-    database: 'postgres',
-    password: '123456'
-  });
+// const pool = new Pool({
+//     host: 'localhost',
+//     user: 'postgres',
+//     port: '5432',
+//     database: 'postgres',
+//     password: '123456'
+//   });
 
 // PARA USAR CON DOTENV
 //   const pool = new Pool({ 
